a6a17bf27b218b818e1031d03359d9a46ca7e3a4
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-19 03:25:05 +0200

    deleting movie unnecessary properties

--- app/src/main/java/com/example/moviesnews/MainActivity.kt
+++ app/src/main/java/com/example/moviesnews/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.moviesnews
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+private const val ID = "id"
+private const val POSTER_PATH = "posterPath"
+private const val BACK_DROP_PATH = "backdropPath"
+private const val TITLE = "title"
+private const val RELEASE_DATE = "releaseDate"
+private const val OVERVIEW = "overview"
+
+class MainActivity : AppCompatActivity(), Callback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,19 +23,25 @@ class MainActivity : AppCompatActivity() {
         val host: NavHostFragment = supportFragmentManager
             .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
 
-        // Set up Action Bar
+
         val navController = host.navController
+
+        setupBottomNavMenu(navController)
 
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//
-//        return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
-//                || super.onOptionsItemSelected(item)
-//    }
+    private fun setupBottomNavMenu(navController: NavController) {
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav?.setupWithNavController(navController)
+
+    }
+
+    override fun onMovieClicked() {
+        val movieIntent = Intent(this, MovieActivity::class.java)
+        movieIntent.putExtra(ID,Utils.itemId)
+        startActivity(movieIntent)
+    }
+
+
 }
