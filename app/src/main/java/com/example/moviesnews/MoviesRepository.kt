175aa973db36d91f8f1e3b41d08395e2a75f6559
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-19 03:23:15 +0200

    deleting movie images

--- app/src/main/java/com/example/moviesnews/MoviesRepository.kt
+++ app/src/main/java/com/example/moviesnews/MoviesRepository.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import api.MovieApi
+import okhttp3.internal.Util
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,6 +15,7 @@ const val BASE_URL = "https://api.themoviedb.org/3/movie/"
 private const val TAG = "MoviesRepository"
 class MoviesRepository {
     private val movieApi:MovieApi
+    private lateinit var detailedMovie: DetailedMovie
     init {
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -80,6 +82,69 @@ class MoviesRepository {
         })
         return responseLiveData
     }
+
+    //to get a movies details
+    fun getMovieDetails() : LiveData<DetailedMovie> {
+        val responseLiveData = MutableLiveData<DetailedMovie>()
+        val moviesRequest = movieApi.getMovieDetails(Utils.itemId)
+        moviesRequest.enqueue(object : Callback<DetailedMovie> {
+            override fun onFailure(call: Call<DetailedMovie>, t: Throwable) {
+                Log.i(TAG, "onFailure: ${t.message.toString()}")
+            }
+
+            override fun onResponse(call: Call<DetailedMovie>, response: Response<DetailedMovie>) {
+                 detailedMovie = response.body()!!
+                responseLiveData.value = detailedMovie
+                Log.i(TAG, "onResponse: $detailedMovie")
+            }
+        })
+
+        return responseLiveData
+    }
+
+    //this function is for fetching  movie recommendations
+    fun getMovieRecommendations () : LiveData<List<Movie>> {
+
+        val responseLiveData = MutableLiveData<List<Movie>>()
+        val recommendedMoviesRequest = movieApi.getMovieRecommendations(Utils.itemId)
+
+        recommendedMoviesRequest.enqueue(object : Callback<MovieResponse>{
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                Log.i(TAG, "onFailure: ${t.message.toString()}")
+            }
+
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                Log.i(TAG, "onResponse: ${response.body()?.results}")
+                val moviesResponseList : List<Movie>? = response.body()?.results
+                responseLiveData.value = moviesResponseList
+            }
+        })
+        return responseLiveData
+    }
+
+    //movieTrailer
+    fun getMovieTrailer () : MutableLiveData<List<Result>> {
+        val trailerMovieRequest = movieApi.getMovieTrailer(Utils.itemId)
+        val responseLiveData = MutableLiveData<List<Result>>()
+
+        var key = ""
+        trailerMovieRequest.enqueue(object : Callback<MovieTrailerModel> {
+            override fun onFailure(call: Call<MovieTrailerModel>, t: Throwable) {
+                Log.i(TAG, "onFailure: ${t.message.toString()}")
+            }
+
+            override fun onResponse(
+                call: Call<MovieTrailerModel>,
+                response: Response<MovieTrailerModel>
+            ) {
+                responseLiveData.value = response.body()?.results
+                Log.i(TAG, "HAHA onResponse: ${response.body()?.results?.size}")
+            }
+        })
+
+        return responseLiveData
+    }
+
 
 
 }
