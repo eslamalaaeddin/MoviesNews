b13435fb29906072a0eadb0d37d31306782b42fc
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-19 03:27:41 +0200

    deleting images live data

--- app/src/main/java/viewmodels/MovieViewModel.kt
+++ app/src/main/java/viewmodels/MovieViewModel.kt
@@ -2,11 +2,13 @@ package viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.example.moviesnews.DetailedMovie
-import com.example.moviesnews.Movie
-import com.example.moviesnews.MoviesRepository
+import com.example.moviesnews.*
 
 class MovieViewModel : ViewModel() {
     private val moviesRepository = MoviesRepository()
     val detailedMovieLiveData : LiveData<DetailedMovie> = moviesRepository.getMovieDetails()
+
+    val recommendedMoviesLiveData : LiveData<List<Movie>> = moviesRepository.getMovieRecommendations()
+    val movieTrailersLiveData : LiveData<List<Result>> = moviesRepository.getMovieTrailer()
+
 }
