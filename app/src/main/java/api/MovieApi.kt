ed3aaf3f50eb0093f6cb1792f62d58d095e8d3a9
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-19 03:26:00 +0200

    deleting get images

--- app/src/main/java/api/MovieApi.kt
+++ app/src/main/java/api/MovieApi.kt
@@ -1,8 +1,9 @@
 package api
 
-import com.example.moviesnews.MovieResponse
+import com.example.moviesnews.*
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MovieApi  {
 
@@ -14,4 +15,14 @@ interface MovieApi  {
 
     @GET("upcoming?api_key=7882262c37362758b7e07865fb261576")
     fun getUpComingMovies():Call<MovieResponse>
+
+    @GET("{movie_id}?api_key=7882262c37362758b7e07865fb261576")
+    fun getMovieDetails( @Path ("movie_id") id:Int):Call<DetailedMovie>
+
+    @GET("{movie_id}/recommendations?api_key=7882262c37362758b7e07865fb261576")
+    fun getMovieRecommendations( @Path ("movie_id") id:Int):Call<MovieResponse>
+
+    @GET("{movie_id}/videos?api_key=7882262c37362758b7e07865fb261576")
+    fun getMovieTrailer(@Path("movie_id")id:Int):Call<MovieTrailerModel>
+
 }
