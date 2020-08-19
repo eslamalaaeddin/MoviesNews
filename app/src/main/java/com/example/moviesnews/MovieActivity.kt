b6495509d018e241ef547964b553de545f61d761
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-19 03:27:09 +0200

    deleting image view model

--- app/src/main/java/com/example/moviesnews/MovieActivity.kt
+++ app/src/main/java/com/example/moviesnews/MovieActivity.kt
@@ -1,28 +1,32 @@
 package com.example.moviesnews
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import fragments.callback
 import viewmodels.MovieViewModel
 
-private const val ID = "id"
-private const val POSTER_PATH = "posterPath"
-private const val BACK_DROP_PATH = "backdropPath"
-private const val TITLE = "title"
-private const val RELEASE_DATE = "releaseDate"
-private const val OVERVIEW = "overview"
 private const val TAG = "MovieActivity"
 class MovieActivity : AppCompatActivity() {
     private lateinit var movieViewModel: MovieViewModel
+    private lateinit var recommendedMoviesAdapter: MoviesAdapter
+    private lateinit var recommendationsRecyclerView: RecyclerView
+
     private  var movie:DetailedMovie ?= null
 
     private lateinit var backPosterImageView: ImageView
@@ -31,11 +35,12 @@ class MovieActivity : AppCompatActivity() {
     private lateinit var movieReleaseDateTextView:TextView
     private lateinit var movieDurationTextView:TextView
     private lateinit var movieOverviewTextView:TextView
+    private lateinit var movieRatingTextView:TextView
 
     private lateinit var watchTrailerButton:Button
-    private lateinit var moreImagesButton: Button
+    private lateinit var moreAboutButton: Button
 
-    private lateinit var recommendationsRecyclerView: RecyclerView
+    private var key = ""
 
     private var itemId = 0
     private var frontPosterPath = ""
@@ -45,6 +50,8 @@ class MovieActivity : AppCompatActivity() {
     private var title = ""
     private var releaseDate = ""
     private var overview = ""
+    private var homePage = ""
+    private var rating = 0.0
     private var duration = 0
 
 
@@ -53,7 +60,6 @@ class MovieActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
         movieViewModel = ViewModelProviders.of(this).get(MovieViewModel::class.java)
-        itemId = intent.getIntExtra(ID,0)
 
         backPosterImageView = findViewById(R.id.back_poster_image_view)
         frontPosterImageView = findViewById(R.id.front_poster_image_view)
@@ -61,20 +67,33 @@ class MovieActivity : AppCompatActivity() {
         movieReleaseDateTextView = findViewById(R.id.movie_release_date_text_view)
         movieDurationTextView = findViewById(R.id.movie_duration_text_view)
         movieOverviewTextView = findViewById(R.id.movie_overview_text_view)
+        movieRatingTextView = findViewById(R.id.moview_rating_text_view)
+        watchTrailerButton = findViewById(R.id.watch_trailer_button)
+        moreAboutButton = findViewById(R.id.more_about_button)
 
+        //recycler view
+        recommendationsRecyclerView = findViewById(R.id.popular_movies_recycler_view)
+        recommendationsRecyclerView.layoutManager =
+            LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        //to get the movie details
         movieViewModel.detailedMovieLiveData.observe(this , Observer {
             backPosterPath = it.backdrop_path
             frontPosterPath = it.poster_path
             title = it.title
+            homePage = it.homepage
+            rating = it.vote_average
             overview = it.overview
             releaseDate = it.release_date
+            duration = it.runtime
+
 
             frontPosterUrl = "https://image.tmdb.org/t/p/w500".plus(frontPosterPath)
             backPosterUrl  = "https://image.tmdb.org/t/p/w500".plus(backPosterPath)
 
             movieTitleTextView.text = title
+            movieRatingTextView.text = rating.toString()
             movieReleaseDateTextView.text = releaseDate
-            movieDurationTextView.text = "${it.runtime} minutes"
+            movieDurationTextView.text = "$duration minutes"
             movieOverviewTextView.text = overview
 
             Picasso.get()
@@ -88,16 +107,85 @@ class MovieActivity : AppCompatActivity() {
                 .into(backPosterImageView)
             Log.i(TAG, "onCreate: ISLAM $movie")
         })
+        //to get a movie recommendations
+        movieViewModel.recommendedMoviesLiveData.observe(this, Observer {
+            recommendedMoviesAdapter = MoviesAdapter(it)
+            recommendationsRecyclerView.adapter = recommendedMoviesAdapter
+        })
+        //to get the key for the trailer video
+        movieViewModel.movieTrailersLiveData.observe(this, Observer {
+            key = it[0].key
+        })
+
+        //open youtube
+        watchTrailerButton.setOnClickListener {
+            startActivity(
+                Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=$key"))
+            )
+        }
+
+        //open the movie's site
+        moreAboutButton.setOnClickListener {
+           val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(homePage))
+            val browserChooserIntent =
+                Intent.createChooser(browserIntent, "Choose browser of your choice")
+            startActivity(browserChooserIntent)
+        }
     }
 
 
-    private fun extractDataFromIntent(){
-        itemId = intent.getIntExtra(ID,0)
-        frontPosterPath = intent.getStringExtra(POSTER_PATH).toString()
-        backPosterPath = intent.getStringExtra(BACK_DROP_PATH).toString()
-        title = intent.getStringExtra(TITLE).toString()
-        releaseDate = intent.getStringExtra(RELEASE_DATE).toString()
-        overview = intent.getStringExtra(OVERVIEW).toString()
+    class MoviesAdapter (private val movies:List<Movie>) :
+        RecyclerView.Adapter<MoviesAdapter.PopularMoviesViewHolder>() {
+
+        inner class PopularMoviesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+            private val movieImageView: ImageView = itemView.findViewById(R.id.movie_image_view)
+
+            init {
+                itemView.setOnClickListener(this)
+            }
+
+            @SuppressLint("SetTextI18n")
+            fun bind(movie: Movie) {
+//                movieTitleTextView.text = movie.title
+//                movieRatingTextView.text = "${movie.vote_average}/10"
+//                movieReleaseDateTextView.text = movie.release_date
+//                movieLanguageTextView.text = movie.original_language
+
+                val url = "https://image.tmdb.org/t/p/w500".plus(movie.poster_path)
+
+                Picasso.get()
+                    .load(url)
+                    .placeholder(R.drawable.ic_loading)
+                    .into(movieImageView)
+            }
+
+            override fun onClick(item: View?) {
+                val movie = movies[adapterPosition]
+                Utils.setMovieDataForIntent(movie.id,
+                    movie.poster_path,
+                    movie.backdrop_path,
+                    movie.title,
+                    movie.release_date,
+                    movie.overview)
+                callback.onMovieClicked()
+            }
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularMoviesViewHolder {
+            val cardView = LayoutInflater.from(parent.context).inflate(R.layout.recommended_movie_item,parent,false) as CardView
+            return PopularMoviesViewHolder(cardView)
+        }
+
+        override fun getItemCount(): Int {
+            return movies.size
+        }
+
+        override fun onBindViewHolder(holder: PopularMoviesViewHolder, position: Int) {
+            val movie = movies[holder.adapterPosition]
+            holder.bind(movie)
+        }
     }
 
 }
