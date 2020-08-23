bc4add7a2ec12832bb341b0192f2d2dd48717375
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-08-23 10:01:17 +0200

    adding favorite view model

---
+++ app/src/main/java/viewmodels/FavoriteMoviesViewModel.kt
@@ -0,0 +1,23 @@
+package viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.moviesnews.DetailedMovie
+import com.example.moviesnews.MoviesRepository
+
+class FavoriteMoviesViewModel : ViewModel() {
+    private val moviesRepository = MoviesRepository()
+    private val favoriteMovies = mutableListOf<DetailedMovie>()
+
+    val favoriteMoviesLiveData : LiveData<MutableList<DetailedMovie>> = liveData {
+        val favoriteMoviesFromDb = moviesRepository.getMovies()
+        for (movie in favoriteMoviesFromDb) {
+            val favMovie = moviesRepository.getFavoriteMovies(movie.movieId)
+            favoriteMovies.add(favMovie)
+        }
+
+        emit(favoriteMovies)
+    }
+
+}
